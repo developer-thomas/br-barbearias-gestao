dfd235cf5ce518369be30c7c3c5bd77936bd77a2
Thomas Edson <andepipous@gmail.com>
2025-08-18 17:25:18 -0400

    feat: create report component

---
+++ src/app/modules/gerencial/pages/reports/reports.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { FranchiseReportComponent } from './franchise-report/franchise-report.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: FranchiseReportComponent,
+  },
+];
