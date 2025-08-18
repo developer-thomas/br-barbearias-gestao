0098334cfea35302e448fbd95253e04bf10450c4
Thomas Edson <andepipous@gmail.com>
2025-08-18 10:36:49 -0400

    feat: create list component

---
+++ src/app/modules/gerencial/pages/banners/list/list.component.ts
@@ -0,0 +1,58 @@
+import { Component, signal } from '@angular/core';
+import { BaseButtonComponent } from '../../../../shared/components/base-button/base-button.component';
+import { CommomTableComponent, TableColumn } from '../../../../shared/components/commom-table/commom-table.component';
+import { FilterTableComponent } from '../../../../shared/components/filter-table/filter-table.component';
+import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [
+    PageHeaderComponent,
+    CommomTableComponent,
+    FilterTableComponent,
+    BaseButtonComponent
+  ],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+  public data = signal<any[]>([]);
+
+  public displayedColumns: TableColumn[] = [
+    { label: 'Data inicial', key: 'initialDate', type: 'text' },
+    { label: 'Data final', key: 'endDate', type: 'text' },
+    { label: 'Nome', key: 'name', type: 'text' },
+    { label: 'Status', key: 'status', type: 'text' },
+    { label: '', key: 'menu', type: 'menu' },
+  ];
+
+  constructor() {
+    this.getData();
+  }
+
+  getData() {
+    const data = [];
+
+    for(let i = 0; i < 10; i++) {
+      data.push({
+        id: i,
+        initialDate: '00/00/00',
+        endDate: '00/00/00',
+        name: 'Nome do banner',
+        status: i % 2 === 0 ? 'Ativo' : 'Desativado'
+      })
+    }
+
+    this.data.set(data);
+  }
+
+  gotoEditPage(row: any) {}
+
+  gotoDetailPage(row: any) {}
+
+  deleteRow(row: any) {}
+
+  
+
+}
