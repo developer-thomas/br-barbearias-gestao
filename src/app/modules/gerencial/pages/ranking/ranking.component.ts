787c3266b96f0ea0859622b8091a9256913bccd0
Thomas Edson <andepipous@gmail.com>
2025-08-18 17:24:38 -0400

    feat: create ranking component

---
+++ src/app/modules/gerencial/pages/ranking/ranking.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-ranking',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  template: '<router-outlet />'
+})
+export class RankingComponent {
+
+}
